f2342e99dde22e985c0a3a29b9c2a499e41cb5ee
backmall <klose4321@gmail.com>
2022-04-18 23:37:58 +0100

    routes

---
+++ routes/auth.routes.js
@@ -0,0 +1,20 @@
+const { verifySignUp } = require("../middleware");
+const controller = require("../controllers/auth.controller");
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Content-Type, Accept"
+    );
+    next();
+  });
+  app.post("/auth/signup",
+    [
+      verifySignUp.isUserUnique,
+      verifySignUp.roleExisting
+    ],
+    controller.signup
+  );
+  app.post("/auth/signin", controller.signin);
+  app.post("/auth/signout", controller.signout);
+};
